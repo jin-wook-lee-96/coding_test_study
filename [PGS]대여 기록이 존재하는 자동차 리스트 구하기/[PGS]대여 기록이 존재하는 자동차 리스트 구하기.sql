2235534977304ff898d53eae48d6b42e7d2adb92
Jin Wook Lee <wksdnr123@naver.com>
2023-03-09 13:47:29 +0900

    first commit

--- [PGS]대여 기록이 존재하는 자동차 리스트 구하기/[PGS]대여 기록이 존재하는 자동차 리스트 구하기.sql
+++ [PGS]대여 기록이 존재하는 자동차 리스트 구하기/[PGS]대여 기록이 존재하는 자동차 리스트 구하기.sql
@@ -1,6 +1,6 @@
-SELECT DIStinct(a.car_id)
-from CAR_RENTAL_COMPANY_RENTAL_HISTORY a, CAR_RENTAL_COMPANY_CAR b
-where a.car_id = b.car_id
-and car_type = '세단'
-and START_DATE BETWEEN '2022-10-01' and '2022-10-31'
-order by a.car_id DESC
+SELECT DISTINCT(a.car_id)
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY a, CAR_RENTAL_COMPANY_CAR b
+WHERE a.car_id = b.car_id
+AND car_type = '세단'
+AND START_DATE BETWEEN '2022-10-01' AND '2022-10-31'
+ORDER BY a.car_id DESC
